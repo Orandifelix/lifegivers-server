bc1f1911b7ea04bc34aac9e79f3d924111a7a62c
Kevin-kinyanjui <kevin.kinyanjui@student.moringaschool.com>
2023-09-03 13:32:37 +0300

    modified users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,42 +1,41 @@
 class UsersController < ApplicationController
-    # Registration action
-    def new
-      @user = User.new
-    end
-  
-    def create
-      @user = User.new(user_params)
-      if @user.save
-        # Log in the user after registration
-        session[:user_id] = @user.id
-        redirect_to root_path, notice: 'Registration successful!'
-      else
-        render :new
-      end
-    end
-  
-    # Login action
-    def login
-      @user = User.find_by(username: params[:username])
-      if @user && @user.authenticate(params[:password])
-        session[:user_id] = @user.id
-        redirect_to root_path, notice: 'Login successful!'
-      else
-        flash.now[:alert] = 'Invalid username or password'
-        render :login
-      end
-    end
-  
-    # Logout action
-    def logout
-      session[:user_id] = nil
-      redirect_to root_path, notice: 'Logged out successfully!'
-    end
-  
-    private
-  
-    def user_params
-      params.require(:user).permit(:username, :password, :name, :gender, :age, :weight, :email, :contact_number, :profile_picture, :medical_history, :address)
+  def create
+    user = User.create(user_params)
+    if user.valid?
+      render json: user, status: :created
+    else
+      render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
     end
   end
+  
+  def show
+    user = User.find_by(id: session[:user_id])
+    if user
+      render json: user
+    else
+      render json: { error: "Not authorized" }, status: :unauthorized
+    end
+  end
+  def login
+      user = User.find_by(username: params[:username])
+      if user && user.authenticate(params[:password])
+        session[:user_id] = user.id
+        render json: user, status: :created
+        # redirect_to root_path, notice: 'Login successful!'
+      else
+        render json: { error: "Invalid username or password" }, status: :unauthorized
+      end
+  end
+  
+  def logout
+      session[:user_id] = nil
+      redirect_to root_path, notice: 'Logged out successfully!'
+  end
+  
+  private
+  
+  def user_params
+      params.require(:user).permit(:username, :password, :name, :gender, :age, :weight, :email, :contact_number, :profile_picture, :medical_history, :address)
+  end
+end
   
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  root 'welcome#index'
+  # root 'welcome#index'
   resources :users, only: [:new, :create]
-  get '/login', to: 'users#login'
+  get "/me", to: "users#show"
   post '/login', to: 'users#login'
   delete '/logout', to: 'users#logout'
 end
