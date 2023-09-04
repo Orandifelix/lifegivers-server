4406e0b82e0264d517bc6a2f46fe86b884d904d6
Kevin-kinyanjui <kevin.kinyanjui@student.moringaschool.com>
2023-09-04 19:13:01 +0300

    done with user backend

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,12 @@
 class UsersController < ApplicationController
-  skip_before_action :authorize, only: [:create]
+  skip_before_action :authorize, only: [:create, :index, :login]
+  skip_before_action :verify_authenticity_token
+
+  def index
+    users = User.all
+    render json: users
+  end
+
   def create
     user = User.create(user_params)
     if user.valid?
@@ -13,20 +20,20 @@ class UsersController < ApplicationController
     user = User.find_by(id: session[:user_id])
     render json: user
   end
+
   def login
       user = User.find_by(username: params[:username])
       if user && user.authenticate(params[:password])
         session[:user_id] = user.id
         render json: user, status: :created
-        # redirect_to root_path, notice: 'Login successful!'
       else
         render json: { error: "Invalid username or password" }, status: :unauthorized
       end
   end
   
   def logout
-      session[:user_id] = nil
-      redirect_to root_path, notice: 'Logged out successfully!'
+    session.delete :user_id
+    head :no_content
   end
   
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'welcome#index'
-  resources :users, only: [:new, :create]
-  get "/me", to: "users#show"
+  resources :users, only: [:create, :index]
+  get '/me', to: 'users#show'
   post '/login', to: 'users#login'
   delete '/logout', to: 'users#logout'
 end
