e6f98072e43aa2804716f647b5875459e7fc1a18
leilaotieno2 <leila.otieno@student.moringaschool.com>
2023-09-05 11:27:32 +0300

    updating the articles controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,2 +1,55 @@
 class ArticlesController < ApplicationController
-end
+    before_action :set_article, only: [:show, :edit, :update, :destroy]
+  
+    def index
+      @articles = Article.all
+    end
+  
+    def show
+    end
+  
+    def new
+      @article = Article.new
+    end
+  
+    def create
+      @article = Article.new(article_params)
+      if @article.save
+        redirect_to @article, notice: 'Article was successfully created.'
+      else
+        render :new
+      end
+    end
+  
+    def edit
+    end
+  
+    def update
+      if @article.update(article_params)
+        redirect_to @article, notice: 'Article was successfully updated.'
+      else
+        render :edit
+      end
+    end
+  
+    def destroy
+      @article.destroy
+      redirect_to articles_url, notice: 'Article was successfully destroyed.'
+    end
+  
+    private
+  
+    def set_article
+      @article = Article.find(params[:id])
+    end
+  
+    def article_params
+      params.require(:article).permit(
+        :user_id,
+        :donation_type_id,
+        :title,
+        :content
+      )
+    end
+  end
+  
