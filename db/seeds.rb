307c0fa1356b0cdd1b5179ae3c316d955f6deb1b
Kevin-kinyanjui <kevin.kinyanjui@student.moringaschool.com>
2023-09-06 14:52:45 +0300

    donation centres seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,46 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+# # Sample data for donation centers
+# donation_centers_data = [
+#   {
+#     name: "St. Michael's Hospital Donation Center",
+#     location: "123 Elm Street",
+#     contact: "5551234567",
+#     operating_hours: "8:00 AM - 6:00 PM"
+#   },
+#   {
+#     name: "Mercy General Hospital Donation Center",
+#     location: "456 Oak Avenue",
+#     contact: "5552345678",
+#     operating_hours: "9:00 AM - 7:00 PM"
+#   },
+#   {
+#     name: "Grace Community Hospital Donation Center",
+#     location: "789 Maple Road",
+#     contact: "5553456789",
+#     operating_hours: "7:30 AM - 5:30 PM"
+#   },
+#   {
+#     name: "Hope Medical Center Donation Center",
+#     location: "101 Pine Lane",
+#     contact: "5554567890",
+#     operating_hours: "8:30 AM - 6:30 PM"
+#   },
+#   {
+#     name: "Unity Regional Hospital Donation Center",
+#     location: "222 Cedar Street",
+#     contact: "5555678901",
+#     operating_hours: "10:00 AM - 8:00 PM"
+#   },
+#   {
+#     name: "Sunrise Memorial Hospital Donation Center",
+#     location: "333 Birch Avenue",
+#     contact: "5556789012",
+#     operating_hours: "7:00 AM - 5:00 PM"
+#   }
+# ]
+
+# # Create donation centers
+# donation_centers_data.each do |data|
+#   DonationCenter.create(data)
+# end
+
+# puts "Seed data for donation centers created successfully."
