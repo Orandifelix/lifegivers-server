2fe77f2270e5f6342d2084513d464ee17b0cbecb
Kevin-kinyanjui <kevin.kinyanjui@student.moringaschool.com>
2023-09-07 19:02:47 +0300

    added validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,15 @@
 class User < ApplicationRecord
     has_secure_password
-    
+  
     validates :username, presence: true, uniqueness: true
     validates :password_digest, presence: true
     validates :name, presence: true
     validates :gender, presence: true
-    validates :age, presence: true, numericality: { only_integer: true }
-    validates :weight, presence: true, numericality: true
+  
+    validates :age, presence: true, numericality: { only_integer: true, greater_than: 18, less_than: 80 }
+    validates :weight, presence: true, numericality: { greater_than: 60 }
     validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
-    validates :contact_number, presence: true
+    validates :contact_number, presence: true, length: { is: 10 }
 end
+  
   
